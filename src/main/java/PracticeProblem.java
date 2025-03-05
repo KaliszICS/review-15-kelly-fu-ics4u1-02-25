fffd2db24b71d4080ca487bdb88a8342a98fe5d6
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-03-05 15:04:12 +0000

    Update with 2 wrong

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,23 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int goodString(String s) {
+        if (s.length() < 2) return -1;  
+        
+		int left = 0;           
+        int right = s.length() - 1;     
 
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                return left + (s.length() - 1 - right); 
+            } 
+            
+            if (s.charAt(left + 1) == s.charAt(right)) {
+                left++;
+            } else {
+                right--; 
+        }
+	}
+        return -1;
+	}
 }

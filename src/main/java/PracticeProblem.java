7094f6da9572d0b6e6c9b0055569ca1070101c1c
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-03-06 03:07:52 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,25 +1,42 @@
+
+import java.util.HashMap;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
-	}
-	public static int goodString(String s) {
-        if (s.length() < 2) return -1;  
-        
-		int left = 0;           
-        int right = s.length() - 1;     
-
-        while (left < right) {
-            if (s.charAt(left) == s.charAt(right)) {
-                return left + (s.length() - 1 - right); 
-            } 
-            
-            if (s.charAt(left + 1) == s.charAt(right)) {
-                left++;
-            } else {
-                right--; 
+public static int goodString(String s){
+    int amount = -1;
+    int length = s.length();
+    int checker = 0;
+    int amountOne =100, amountTwo =100;
+    
+    HashMap <Character,Integer> map = new HashMap<Character, Integer>();
+    for (int i = 0; i < length; i ++){
+        map.put((s.charAt(i)),i);
+    }
+    for (int index = 0; index < length; index++){
+        if (map.get(s.charAt(index)) != index && checker == 0){
+            amountOne = (s.length()-1) - map.get(s.charAt(index)) + index;
+            checker = index;
         }
-	}
-        return -1;
-	}
+        if (map.get(s.charAt(index)) != index && index > checker){
+            amountTwo = (s.length()-1) - map.get(s.charAt(index)) + index;
+            length = 0;
+        }
+    }
+        
+    if (amountOne<amountTwo){
+        amount = amountOne;
+    }
+    else if (amountOne+amountTwo==200){
+        amount = -1;
+    }
+    else{
+    amount = amountTwo;
+    }
+
+    return amount;
+
+}
+
+
 }
